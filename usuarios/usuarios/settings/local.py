c5877250c1b36fcf73c435b1c8fa5a3a58207f34
Maizares, Juan <juan_maizares386@hotmail.com>
2022-04-09 17:39:50 -0300

    cambiar datos de acceso de config por var de entorno

--- usuarios/usuarios/settings/local.py
+++ usuarios/usuarios/settings/local.py
@@ -10,9 +10,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbuser',
-        'USER': 'neunapp',
-        'PASSWORD': 'neunapp',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
