efd4409a557dad5a968a399672e71322563cc0c6
Maizares, Juan <juan_maizares386@hotmail.com>
2022-04-18 21:52:45 -0300

    registro de user

--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -10,15 +10,18 @@ class User(AbstractBaseUser, PermissionsMixin):
         ('F', 'Femenino'),
         ('O', 'Otros'),
     )
+    #campos
     username = models.CharField(max_length=10, unique=True)
     email = models.EmailField()
     nombres = models.CharField(max_length=30, blank=True)
     apellidos = models.CharField(max_length=30, blank=True)
     genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
     is_staff = models.BooleanField(default=False)
+    #configuraciones
     USERNAME_FIELD = 'username'
     objects = UserManager()
     REQUIRED_FIELDS = ['email',]
+    #metodos
     def get_short_name(self):
         return self.username
     def get_full_name(self):
--- usuarios/applications/users/views.py
+++ usuarios/applications/users/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 from django.views.generic import (
     CreateView   
 )
+from django.views.generic.edit import (
+    FormView   
+)
 from .forms import UserRegisterForm
+from .models import User
 
-class UserRegisterView(CreateView):
+class UserRegisterView(FormView):
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = '/'
+    
+    def form_valid(self, form):
+        # Modelo User usa, un manager, que tiene contructores.
+        #estos manager tiene a _create_user(), ahi pasamos los params para instanciar.
+        #si tenemos params que no esta mencionados en los contructres, los recibimos como extra_ields
+        # y los mandamos, como aca, por palabra clave.(nombres, apellidos y genero)
+        User.objects.create_user(
+            form.cleaned_data['username'],
+            form.cleaned_data['email'],
+            form.cleaned_data['password1'],
+            #extra_fields: campos no nombranos ind en los metodos contructores.
+            nombres = form.cleaned_data['nombres'],
+            apellidos = form.cleaned_data['apellidos'],
+            genero = form.cleaned_data['genero'],
+        )
+        return super(UserRegisterView, self).form_valid(form)
