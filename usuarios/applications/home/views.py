15e9ec1d5aeea5053e74c4dd3e989fbaf1c11b81
Maizares, Juan <juan_maizares386@hotmail.com>
2022-04-19 22:47:51 -0300

    proceso de logeo y logout

--- usuarios/applications/home/views.py
+++ usuarios/applications/home/views.py
@@ -1,3 +1,8 @@
+from re import template
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import (TemplateView)
+
+class HomePage(TemplateView):
+    template_name = "home/index.html"
--- usuarios/applications/users/forms.py
+++ usuarios/applications/users/forms.py
@@ -33,3 +33,26 @@ class UserRegisterForm(forms.ModelForm):
     def clean_password2(self):
         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
             self.add_error('password2', 'Las contraseñas no son iguales')
+
+class LoginForm(forms.Form):
+    """LoginForm definition."""
+
+    # TODO: Define form fields here
+    username = forms.CharField(
+        label='Usuario',
+        required=True,
+        #para personalizar 
+        widget=forms.TextInput(
+            attrs= {
+                'placeholder': 'Usuario'
+            }
+    ))
+    password = forms.CharField(
+        label='Contraseña',
+        required=True,
+        #para personalizar 
+        widget=forms.PasswordInput(
+            attrs= {
+                'placeholder': 'Contraseña'
+            }
+    ))
